2e6d5ab710ea7ad260a0d4c2098fbf54d6142c2d
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-27 21:45:10 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,8 @@ impl Compiler {
         let top = "section .text\n\tglobal _start\n\n_start:\n";
         let exit = "\tmov rdi, rax\n\tmov rax, 0x2000001\n\tsyscall\n";
         let vars = compiler.variables.into_iter().collect::<String>();
-        Some(format!("{top}{code}\n{exit}\nsection .data\n{vars}"))
+        let fnc = compiler.functions.into_iter().collect::<String>();
+        Some(format!("{top}{code}\n{exit}\n{fnc}\nsection .data\n{vars}"))
     }
 }
 
