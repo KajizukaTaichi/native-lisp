068835085c9a48797730b10f22226d2109d59aa9
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-27 21:30:56 +0900

    Create parse.rs

---
+++ src/parse.rs
@@ -0,0 +1,28 @@
+use crate::*;
+
+impl Expr {
+    pub fn parse(source: &str) -> Option<Self> {
+        let source = source.trim();
+        if let Some(Some(source)) = source.strip_prefix("(").map(|x| x.strip_suffix(")")) {
+            Some(Expr::List(
+                tokenize(source)?
+                    .iter()
+                    .map(|x| Expr::parse(x))
+                    .collect::<Option<Vec<_>>>()?,
+            ))
+        } else {
+            Some(Expr::Atom(Atom::parse(source)?))
+        }
+    }
+}
+
+impl Atom {
+    pub fn parse(source: &str) -> Option<Self> {
+        let source = source.trim();
+        if let Ok(n) = source.parse::<i64>() {
+            Some(Atom::Integer(n))
+        } else {
+            Some(Atom::Symbol(source.to_string()))
+        }
+    }
+}
